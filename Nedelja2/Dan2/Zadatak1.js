530e1d517fe1ceb2a1198c1c7a0a486a9017e70c
Aleksandar Matovic <56871566+aleksandarmatovic1@users.noreply.github.com>
2019-10-30 22:01:05 +0100

    Dan2

---
+++ Nedelja2/Dan2/Zadatak1.js
@@ -0,0 +1,12 @@
+var x=6;
+if (6/3); {
+    console.log (broj 6 je deljiv 3);
+}
+else {
+    console.log ("broj 6 nije deljiv sa 3");
+}
+
+// Drugi dan
+// Prvi zadatak.js
+// ceo broj deljiv ili nedeljiv sa 3
+
