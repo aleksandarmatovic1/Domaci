0bdfa41a1e2e875d50bafe83bf84aae24bba2ea3
Aleksandar Matovic <56871566+aleksandarmatovic1@users.noreply.github.com>
2019-10-30 23:18:52 +0100

    Dan 2

---
+++ Nedelja2/Dan2/Zadatak2.js
@@ -0,0 +1,11 @@
+
+var x=0
+var y=1000
+for (var counter = 0; counter <= 1000; counter++) {
+    if( counter % 5 == 0) {
+         console.log( counter )
+    }
+ }
+
+ // Dan2 Drugi zadatak
+ //Ispisati prvih 1000 brojeva deljivih sa 5 
