dcda0d58f20105e54956648ba894e384d5465f68
Aleksandar Matovic <56871566+aleksandarmatovic1@users.noreply.github.com>
2019-10-30 23:47:56 +0100

    Dan 1

---
+++ Nedelja2/Dan1/Zadatak1.js
@@ -0,0 +1,34 @@
+//sabiranje
+
+function addition(a, b) {
+ return a+b
+}
+addition(1, 2);
+
+
+
+//oduzimanje
+
+function subtraction(a, b) {
+ return a-b
+}
+subtraction(3, 2);
+
+
+
+//mnozenje
+
+function multiplication(a, b) {
+ return a*b
+}
+multiplication (3, 2);
+
+
+//deljenje
+
+function divide(a, b) {
+ return a/b
+}
+divide (10, 2);
+
+//Dan 1 - Prvi zadatak
