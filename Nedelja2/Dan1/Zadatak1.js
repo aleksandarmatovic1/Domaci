c28d7550c3c0acaa24be02197c65e1801f5662fe
Aleksandar Matovic <56871566+aleksandarmatovic1@users.noreply.github.com>
2019-10-31 03:35:03 +0100

    Dan 1

--- Nedelja2/Dan1/Zadatak1.js
+++ Nedelja2/Dan1/Zadatak1.js
@@ -1,5 +1,12 @@
 //sabiranje
 
+var a = 1;
+var b = 2;
+var c=a+b;
+console.log(c);
+
+//Malo sam istrazivao pa sam naisao na jos neki nacin i odlucio da napisem i njega. 
+
 function addition(a, b) {
  return a+b
 }
@@ -9,14 +16,27 @@ addition(1, 2);
 
 //oduzimanje
 
+var a = 3;
+var b = 2;
+var c=a-b;
+console.log(c);
+
+//Nacin 2.
+
 function subtraction(a, b) {
  return a-b
 }
 subtraction(3, 2);
 
 
-
 //mnozenje
+
+var a = 3;
+var b = 2;
+var c=a*b;
+console.log(c);
+
+//Nacin 2
 
 function multiplication(a, b) {
  return a*b
@@ -25,6 +45,13 @@ multiplication (3, 2);
 
 
 //deljenje
+
+var a = 10;
+var b = 2;
+var c=a/b;
+console.log(c);
+
+//Nacin 2
 
 function divide(a, b) {
  return a/b
