01913504854e24fb76ffbe47a5273f1c1b12ffc3
Aleksandar Matovic <56871566+aleksandarmatovic1@users.noreply.github.com>
2019-11-03 19:43:57 +0100

    Dan 3

---
+++ Nedelja2/Dan 3/Zadatak 2.js
@@ -0,0 +1,22 @@
+
+var numberValue = 3;
+var numberValue = 5;
+
+for (var counter = 0; counter <= 100; counter++) {
+    if (counter % 3 == 0);
+    else if (counter % 5 == 0); {
+        console.log('FizzBuzz');
+    }
+    var x = 3;
+    for (var counter = 0; counter <= 100; counter++) {
+        if (counter % 3 == 0) {
+            console.log('Fizz');
+        }
+        var x = 5;
+        for (var counter = 0; counter <= 100; counter++) {
+            if (counter % 5 == 0) {
+                console.log('Buzz')
+                    
+                {
+                    console.log('Broj nije deljiv sa 3');
+                }
